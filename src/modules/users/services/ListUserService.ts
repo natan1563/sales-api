1b325c1709c3b025af2347ce58c96afa230fe57e
natan1563 <natromafe@gmail.com>
2022-12-21 22:49:38 -0300

    service to list users

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,10 @@
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UsersRepository from "../typeorm/repositories/UsersRepository";
+
+export default class ListUserService {
+  public async execute(): Promise<User[]> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    return usersRepository.find();
+  }
+}
