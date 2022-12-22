366bd49f71a6e1660b216f77664d54f1aa1131f0
natan1563 <natromafe@gmail.com>
2022-12-21 22:32:26 -0300

    user repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,21 @@
+import { EntityRepository, Repository } from "typeorm";
+import User from "../entities/User";
+
+@EntityRepository(User)
+export default class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    return await this.findOne({
+      where: { name }
+    })
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    return await this.findOne(id);;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    return await this.findOne({
+      where: { email }
+    })
+  }
+}
