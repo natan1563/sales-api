159d210f74d43c23049653777632fc9766aebf5c
natan1563 <natromafe@gmail.com>
2023-01-16 23:47:05 -0300

    refactoring ethereal mail and create parser

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -2,5 +2,9 @@ export default {
   jwt: {
     secret: 'f1e915ff831db06cf3fcb895fca51f63',
     expiresIn: '1d'
+  },
+  ethereal: {
+    user:'evert.toy40@ethereal.email',
+    pass: 'NYfw6p7Xx7MkaTqsJc',
   }
 }
--- src/config/mail/EtherealMail.ts
+++ src/config/mail/EtherealMail.ts
@@ -1,27 +1,54 @@
 import nodemailer from "nodemailer";
-interface ISendMail {
-  to: string;
-  body: string;
+import authConfig from '../auth';
+import HandlebarsMailTemplate from "./HandlebarsMailTemplate";
+
+interface ITemplateVariables {
+  [key: string]: string | number
 }
 
+interface IParseMailTemplate {
+  template: string;
+  variables: ITemplateVariables;
+}
+
+interface IMailContact {
+  name: string;
+  email: string;
+}
+
+interface ISendMail {
+  from?: IMailContact;
+  to: IMailContact;
+  templateData: IParseMailTemplate;
+  subject: string;
+}
+
+
 export default class Etherealmail {
-  public static async sendMail({ to, body }: ISendMail ): Promise<void> {
+  public static async sendMail({ to, from, subject, templateData }: ISendMail ): Promise<void> {
     const account = await nodemailer.createTestAccount();
     const transporter = nodemailer.createTransport({
       host: account.smtp.host,
       port: account.smtp.port,
       secure: account.smtp.secure,
       auth: {
-        user:'evert.toy40@ethereal.email',
-        pass: 'NYfw6p7Xx7MkaTqsJc',
+        ...authConfig.ethereal,
       },
     });
 
+    const mailTemplate = new HandlebarsMailTemplate();
+
     const message = await transporter.sendMail({
-      from: 'equipe@apivendas.com',
-      to,
-      subject: 'Recovery of your password',
-      text: body
+      from: {
+        name: from?.name || 'Equipe API Vendas',
+        address: from?.email || 'equipe@apivendas.com'
+      },
+      to: {
+        name: to.name,
+        address: to.email,
+      },
+      subject,
+      html: await mailTemplate.parser(templateData)
     })
 
     console.log('Message sent: %s', message.messageId);
