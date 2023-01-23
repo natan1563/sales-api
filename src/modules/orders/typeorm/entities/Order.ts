5ce531855cf624e1d5a17079c2a0e244b9e6696c
natan1563 <natromafe@gmail.com>
2023-01-23 20:33:35 -0300

    Order entity

---
+++ src/modules/orders/typeorm/entities/Order.ts
@@ -0,0 +1,25 @@
+import Customer from "@modules/customers/typeorm/entities/Customer";
+import {
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
+
+@Entity('orders')
+export default class Order {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => Customer)
+  @JoinColumn({ name: 'customer_id' })
+  customer: Customer;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
