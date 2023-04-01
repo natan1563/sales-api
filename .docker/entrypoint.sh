77c281f99bd801ad303f7ab51ba3492f8eebee78
natan1563 <natromafe@gmail.com>
2023-03-31 21:06:07 -0300

    fix runnable on docker

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 rm -rf node_modules
 npm install
+npm cache clean --force
+chmod +x node_modules/@babel/cli/bin/babel.js
+npm run build
 npm run typeorm migration:run
 npm run dev
--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ RUN apk add --no-cache bash
 
 USER node
 
+RUN mkdir -p /home/node/app
+RUN chmod -R 777 /home/node/app
 WORKDIR /home/node/app
